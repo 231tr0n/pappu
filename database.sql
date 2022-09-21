9cdf4632141dd06eddd56525d2a838dfc3dc74ec
231tr0n <zeltronsrikar@gmail.com>
2022-09-21 12:35:29 +0530

    test

--- database.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER IF NOT EXISTS 'pappu'@'localhost' IDENTIFIED BY '********';
-ALTER USER 'pappu'@'localhost' IDENTIFIED BY '*******';
-CREATE DATABASE IF NOT EXISTS pappu;
-GRANT ALL PRIVILEGES ON pappu.* TO 'pappu'@'localhost';
-flush privileges;
--- next.md
+++
@@ -1,1 +0,0 @@
-Create a command to remove a user from the pentest google drive and also create a field for gmail.
