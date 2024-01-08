cdc0eca10341c2836b8f0e89ea81fb1fedf24ee6
231tr0n <zeltronsrikar@gmail.com>
2024-01-08 21:33:27 +0530

    config

--- src/models.js
+++ src/models.js
@@ -9,6 +9,9 @@ models.statuses = Object.freeze({
 });
 
 models.status_return_type = (update, holiday) => {
+  if (!update || !holiday || !Array.isArray(update) || !Array.isArray(update)) {
+    throw new Error('parameters are mandatory and should be arrays');
+  }
   this.update = update;
   this.holiday = holiday;
 };
@@ -19,9 +22,12 @@ models.setup = async () => {
   );
 };
 
-models.upsert_date = async (date) => {
+models.insert_date = async (date) => {
+  if (date && !Date.parse(date)) {
+    throw new Error('wrong date provided');
+  }
   let results = null;
-  if (date && Date.parse(date)) {
+  if (date) {
     results = database.query('SELECT * FROM `status_updates` WHERE date = ?', [
       date,
     ]);
@@ -33,7 +39,7 @@ models.upsert_date = async (date) => {
   if (results) {
     return;
   }
-  if (date && Date.parse(date)) {
+  if (date) {
     await database.query('INSERT INTO `status_updates` (`date`) VALUES (?)', [
       date,
     ]);
@@ -52,54 +58,99 @@ models.delete_date = async (date) => {
   await database.query('DELETE FROM `status_updates` WHERE `date` = (date())');
 };
 
-models.upsert_status = async (id, status, date) => {
-  if (date && Date.parse(date)) {
-    await models.upsert_date(date);
-    await database.query('UPDATE `status_updates` SET ? = ? WHERE date = ?', [
-      id,
-      status,
-      date,
-    ]);
-    return;
-  }
-  await models.upsert_date();
-  await database.query(
-    'UPDATE `status_updates` SET ? = ? WHERE date = (date())',
-    [id, status],
-  );
-};
-
-models.update_status = async (id, status, date) => {
+models.insert_status = async (id, status, date) => {
   if (date && !Date.parse(date)) {
-    throw new Error('Wrong date provided');
+    throw new Error('wrong date provided');
   }
-  if (status !== models.statuses.holiday || status !== models.statuses.update !== models.statuses.no_update) {
+  if (status !== models.statuses.holiday || status !== models.statuses.update) {
     throw new Error('invalid status');
   }
+  await models.insert_date(date);
   let res = null;
   if (date) {
     res = await models.get_status_of_id_on_date(id, date);
   } else {
     res = await models.get_status_of_id_on_date(id);
   }
-  if (res === status) {
+  if (res === models.statuses.update || res === models.statuses.holiday) {
     return;
   }
-  const ret = await models.get_statuses_on_date(date);
   if (date) {
-    await database.query('UPDATE `status_updates` SET ? = ? WHERE date = ?', [
-      id,
-      status,
-      date,
-    ]);
+    res = await models.get_statuses_on_date(date);
+  } else {
+    res = await models.get_statuses_on_date();
   }
-  await database.query(
-    'UPDATE `status_updates` SET ? = ? WHERE date = (date())',
-    [id, status],
-  );
+  if (status === models.statuses.update) {
+    res.update += `,${id}`;
+  } else {
+    res.holiday += `,${id}`;
+  }
+  if (date) {
+    await models.set_statuses_on_date(res, date);
+    return;
+  }
+  await models.set_statuses_on_date(res);
+};
+
+models.upsert_status = async (id, status, date) => {
+  if (date && !Date.parse(date)) {
+    throw new Error('wrong date provided');
+  }
+  if (status !== models.statuses.holiday || status !== models.statuses.update !== models.statuses.no_update) {
+    throw new Error('invalid status');
+  }
+  await models.insert_date(date);
+  let ret = null;
+  if (date) {
+    ret = await models.get_status_of_id_on_date(id, date);
+  } else {
+    ret = await models.get_status_of_id_on_date(id);
+  }
+  if (ret === status) {
+    return;
+  }
+  if (date) {
+    ret = await models.get_statuses_on_date(date);
+  } else {
+    ret = await models.get_statuses_on_date();
+  }
+  if (status === models.statuses.update) {
+    ret.holiday = ret.holiday.split(',').filter((item) => item !== id).join(',');
+    ret.update += `,${id}`;
+  } else if (status === models.statuses.holiday) {
+    ret.update = ret.update.split(',').filter((item) => item !== id).join(',');
+    ret.holiday += `,${id}`;
+  } else {
+    ret.update = ret.update.split(',').filter((item) => item !== id).join(',');
+    ret.holiday = ret.holiday.split(',').filter((item) => item !== id).join(',');
+  }
+  if (date) {
+    await models.set_statuses_on_date(ret, date);
+    return;
+  }
+  await models.set_statuses_on_date(ret);
+};
+
+models.set_statuses_on_date = async (statuses, date) => {
+  if (date && !Date.parse(date)) {
+    throw new Error('wrong date provided');
+  }
+  if (!statuses || !statuses.update || !statuses.holiday || !Array.isArray(statuses.update) || !Array.isArray(statuses.holiday)) {
+    throw new Error('wrong object provided');
+  }
+  if (date) {
+    await models.delete_date(date);
+    await database.query('INSERT INTO `status_updates` (`date`, `update`, `holiday`) VALUES (?, ?, ?)', [date, statuses.update.join(','), statuses.holiday.join(',')]);
+    return;
+  }
+  await models.delete_date();
+  await database.query('INSERT INTO `status_updates` (`date`, `update`, `holiday`) VALUES (date(), ?, ?)', [statuses.update.join(','), statuses.holiday.join(',')]);
 };
 
 models.get_statuses_on_date = async (date) => {
+  if (date && !Date.parse(date)) {
+    throw new Error('wrong date provided');
+  }
   let results = null;
   if (date) {
     results = await database.query(
@@ -123,7 +174,7 @@ models.get_statuses_on_date = async (date) => {
 
 models.get_status_of_id_on_date = async (id, date) => {
   if (date && !Date.parse(date)) {
-    throw new Error('Wrong date provided');
+    throw new Error('wrong date provided');
   }
   let results = null;
   if (date) {
